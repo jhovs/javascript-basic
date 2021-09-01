636a200a2897606ee7458c8d5d18610ea35d1cbb
jhovan <jhovan.solano@gmail.com>
2021-08-31 21:47:55 -0500

    Primer archivo de clase explicando tipos de valores

---
+++ valores.js
@@ -0,0 +1,20 @@
+/* ## Valores Primitivos ## */
+// valor tipo numero
+40;
+
+// valor tipo string 
+"Jhovan solano";
+
+// valor booleano 
+true;
+false;
+
+//valores bacios 
+null;
+undefined;
+
+
+/* valores tipo objetos */
+//array
+ [1,2,3];
+{ nombre: "Jhovan"}
