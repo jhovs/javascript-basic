b6eaf33753932c0260074020b73b744fab8ee756
jhovan <jhovan.solano@gmail.com>
2021-09-01 12:24:02 -0500

    clase 05 funciones en JavaScript

---
+++ variables.js
@@ -0,0 +1,21 @@
+//Representación en donde vamos a guardar valores
+var nombre = "Jhovan";
+
+//Dos estado de una variable 
+//declarar variable
+var edad;
+
+//inicializar variable 
+edad = 30;
+
+
+//array
+var elementos = ["computadora", "celular", "laptop"];
+
+//tipo objeto 
+var persona = {
+    nombre: "Jhovan",
+    edad: 30
+}
+
+persona;
