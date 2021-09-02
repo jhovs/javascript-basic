78ab68a3bd3d4d75201d366a9c87f16c362a227c
jhovan <jhovan.solano@gmail.com>
2021-09-01 20:27:54 -0500

    clase 05 comlemento

---
+++ funciones.js
@@ -0,0 +1,34 @@
+//hay dos tipos de funciones 
+
+//Declarativas 
+function miFuncion1(){
+    return 3;
+}
+
+miFuncion1();
+
+//Expresión se les conose tambien como variables anonimas
+var miFuncion2 = function(a,b){
+    return a + b;
+}
+
+miFuncion2();
+
+
+//funcion declarativa 
+function saludarEstudiantes(estudiante){
+    console.log(`Hola ${estudiante}`)
+}
+
+saludarEstudiantes("Jhovan");
+
+//--> 
+function sumar(a,b){
+    var resultado = a + b;
+    return resultado;
+}
+
+
+function sumar(a,b){
+    return a + b;
+}
